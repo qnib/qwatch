6294cbd4de847159a0add2bce8e55525b5f3f829
Christian Kniep <christian@qnib.org>
2016-11-26 13:57:18 +0100

    update vendor.json and only output coverage

--- test.sh
+++ test.sh
@@ -3,8 +3,5 @@
 for x in cmd collectors handlers server types utils;do
     pushd ${x} >>/dev/null
     go test -coverprofile=coverage.out
-    if [ -f coverage.out ];then
-        go tool cover -func=coverage.out -o ../resources/coverage/${x}.html
-    fi
     popd >>/dev/null
 done
