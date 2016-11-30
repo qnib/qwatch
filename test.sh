ce9e763f4e659d99188cb5c4ef0ed70a0e2bece4
Christian Kniep <ChristianKniep@users.noreply.github.com>
2016-11-30 21:05:59 +0100

    Beef test (#11)
    * make test great again
    * update qnib/qwatch/output

--- test.sh
+++ test.sh
@@ -5,5 +5,13 @@ echo "> govendor remove +unused"
 govendor remove +unused
 echo "> govendor sync"
 govendor sync
-
-gocov test ./... || exit 0
+if [ ! -d resources/coverity ];then
+    mkdir -p resources/coverity
+fi
+go test -coverprofile=coverage.out
+for x in $(find . -maxdepth 1 -type d |egrep -v "(\.$|\.git|vendor|bin|resources)");do
+    go test -coverprofile=resources/coverity/${x}.out ${x} >>/dev/null
+done
+coveraggregator -o resources/coverity/coverage-all.out $(find . -name coverage.out |egrep -v "(\.$|\.git|vendor|bin)") >>/dev/null
+go tool cover -func=resources/coverity/coverage-all.out |tee ./coverage-all.out
+go tool cover -html=resources/coverity/coverage-all.out -o resources/coverity/all.html
