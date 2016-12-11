6d1606a0a874a531177053bedc5d7b8c9beb7f17
Christian Kniep <christian@qnib.org>
2016-12-11 15:42:38 +0100

    add droneCI + badge in README

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xe
 
 govendor fetch +missing
 echo "> govendor remove +unused"
@@ -8,10 +9,12 @@ govendor sync
 if [ ! -d resources/coverity ];then
     mkdir -p resources/coverity
 fi
-go test -coverprofile=coverage.out
+go test -cover -coverprofile=qwatch.cover >>/dev/null
+COVER_FILES="qwatch.cover"
 for x in $(find . -maxdepth 1 -type d |egrep -v "(\.$|\.git|vendor|bin|resources)");do
-    go test -coverprofile=resources/coverity/${x}.out ${x} >>/dev/null
+    go test -cover -coverprofile=resources/coverity/${x}.cover ${x} >>/dev/null
+    COVER_FILES="${COVER_FILES} resources/coverity/${x}.cover"
 done
-coveraggregator -o resources/coverity/coverage-all.out $(find . -name coverage.out |egrep -v "(\.$|\.git|vendor|bin)") >>/dev/null
-go tool cover -func=resources/coverity/coverage-all.out |tee ./coverage-all.out
-go tool cover -html=resources/coverity/coverage-all.out -o resources/coverity/all.html
+coveraggregator -o coverage-all.out ${COVER_FILES} >>/dev/null
+#go tool cover -func=coverage-all.outcover |tee ./resources/coverity/coverage-all.out
+#go tool cover -html=coverage-all.out -o resources/coverity/all.html
