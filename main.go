b3e1dff1ba7b42cb5609b55b3c54f4fe8ccf3462
Christian Kniep <christian@qnib.org>
2017-02-03 18:40:29 +0100

    Bump version: 0.7.1.3 → 0.7.1.4

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "qwatch"
 	app.Usage = ""
-	app.Version = "0.7.1.3"
+	app.Version = "0.7.1.4"
 	app.Commands = []cli.Command{cmd.ServerCmd}
 	app.Run(os.Args)
 
