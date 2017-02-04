74a93f67a07e50b6efc78547eec7cc364a60abed
Christian Kniep <ChristianKniep@users.noreply.github.com>
2017-02-04 09:28:46 +0100

    add ImageName and parsing (#35)

--- utils/utils.go
+++ utils/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/grafov/bcast"
 	"github.com/qnib/qwatch/types"
@@ -25,4 +26,31 @@ func NewChannels() qtypes.Channels {
 		Tick:      bcast.NewGroup(), // create broadcast group
 		Done:      make(chan os.Signal, 1),
 	}
+}
+
+//ParseImageName splits Docker image names
+func ParseImageName(i string) qtypes.ImageName {
+	// Should use regex, but let's start silly
+	in := qtypes.ImageName{}
+	sha := strings.Split(i, "@")
+	if len(sha) != 1 {
+		in.Sha256 = sha[1][7:]
+		i = sha[0]
+	}
+	sTag := strings.Split(i, ":")
+	if len(sTag) != 1 {
+		in.Tag = sTag[1]
+		i = sTag[0]
+	} else if in.Sha256 == "" {
+		in.Tag = "latest"
+	}
+	l := strings.Split(i, "/")
+	in.Name, l = l[len(l)-1], l[:len(l)-1]
+	if len(l) > 0 {
+		in.Repository, l = l[len(l)-1], l[:len(l)-1]
+	}
+	if len(l) > 0 {
+		in.Registry = l[len(l)-1]
+	}
+	return in
 }
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -1,1 +1,33 @@
-package utils_test
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/qnib/qwatch/types"
+)
+
+func TestParseImageName(t *testing.T) {
+	// image
+	got := ParseImageName("image")
+	exp := qtypes.ImageName{Name: "image", Tag: "latest"}
+	assert.Equal(t, exp, got)
+	// image@sha256:abc
+	got = ParseImageName("image@sha256:sha")
+	exp = qtypes.ImageName{Name: "image", Sha256: "sha"}
+	assert.Equal(t, exp, got)
+	// repo/image@sha256:abc
+	got = ParseImageName("repo/image@sha256:sha")
+	exp = qtypes.ImageName{Repository: "repo", Name: "image", Sha256: "sha"}
+	assert.Equal(t, exp, got)
+	// registry/repo/image@sha256:sha
+	got = ParseImageName("registry/repo/image@sha256:sha")
+	exp = qtypes.ImageName{Registry: "registry", Repository: "repo", Name: "image", Sha256: "sha"}
+	assert.Equal(t, exp, got)
+	// registry/repo/image:tag
+	got = ParseImageName("registry/repo/image:tag")
+	exp = qtypes.ImageName{Registry: "registry", Repository: "repo", Name: "image", Tag: "tag"}
+	assert.Equal(t, exp, got)
+
+}
